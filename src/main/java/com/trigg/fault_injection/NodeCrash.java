e8c222f09b71b23aae9a78757b246a7321508ce8
Ciara Trigg <ciaratrigg20@gmail.com>
2025-02-22 12:10:14 -0500

    node crash entity class

--- src/main/java/com/trigg/fault_injection/NodeCrash.java
+++ src/main/java/com/trigg/fault_injection/NodeCrash.java
@@ -1,5 +1,13 @@
 package com.trigg.fault_injection;
 
 public class NodeCrash extends Fault{
+    int num_nodes;
 
+    public int getNum_nodes() {
+        return num_nodes;
+    }
+
+    public void setNum_nodes(int num_nodes) {
+        this.num_nodes = num_nodes;
+    }
 }
